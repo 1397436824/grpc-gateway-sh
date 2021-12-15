a4191e90747008a670ef1602013171ec4277a11b
Ouyang Kang <www.1397436824@qq.com>
2021-12-15 10:31:58 +0800

    add install-grpc-gateway.sh

---
+++ install-grpc-gateway.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+GOPATH=`go env GOPATH`
+
+rm -rf $GOPATH/src/github.com/grpc-ecosystem/grpc-gateway
+mkdir -p $GOPATH/src/github.com/grpc-ecosystem
+cd $GOPATH/src/github.com/grpc-ecosystem
+wget https://github.com/grpc-ecosystem/grpc-gateway/archive/v2.0.0.zip -O grpc-gateway.zip
+unzip grpc-gateway.zip
+rm -f grpc-gateway.zip
+mv grpc-gateway-2.0.0 grpc-gateway

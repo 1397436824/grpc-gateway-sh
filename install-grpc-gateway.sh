ac926f06d265a7683f2ec1f2692000f2c4bb9b06
1397436824 <59243085+1397436824@users.noreply.github.com>
2021-12-15 10:58:58 +0800

    Update install-grpc-gateway.sh
    add install protoc-gen-grpc-gateway

--- install-grpc-gateway.sh
+++ install-grpc-gateway.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "start..."
+
 GOPATH=`go env GOPATH`
 
 rm -rf $GOPATH/src/github.com/grpc-ecosystem/grpc-gateway
@@ -9,3 +11,8 @@ wget https://github.com/grpc-ecosystem/grpc-gateway/archive/v2.0.0.zip -O grpc-g
 unzip grpc-gateway.zip
 rm -f grpc-gateway.zip
 mv grpc-gateway-2.0.0 grpc-gateway
+
+cd grpc-gateway/protoc-gen-grpc-gateway/
+go build go install
+
+echo "finish."
